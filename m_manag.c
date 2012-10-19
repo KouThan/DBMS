21b15ccf971c32b8ad98019ef625a1068d8f15db
KouThan <thanasiskount@gmail.com>
2012-10-19 17:57:13 +0300

    Structs, init and some file functions implemented
    Added structs initialisation function, create and destroy file
    functions. Added more error defines.

--- m_manag.c
+++ m_manag.c
@@ -2,33 +2,96 @@
 #include <stdlib.h>
 #include "memory_management.h"
 
-#define BLE_OK 0
-#define BLE_NOMEM -1
-#define BLE_NOBUF -2
-#define BLE_BLOCKFIXED -3
-#define BLE_BLOCKNOTINBUF -4
-#define BLE_BLOCKINBUF -5
-#define BLE_OS -6
-#define BLE_INVALIDBLOCK -7
-#define BLE_FILEOPEN -8
-#define BLE_FTABFULL -9
-#define BLE_OFTABFULL -10
-#define BLE_FD -11
-#define BLE_EOF -12
-#define BLE_BLOCKFREE -13
-#define BLE_BLOCKUNFIXED -14
 
-int BL_init(void) {
 
+int BL_init(void) { 
+    int i,j;
+    for(i=0;i<openingSize;i++){ 
+        appOpenings[i]=EMPTY; 
+        OpenFile[i].fileHandler=NULL;   
+        CacheArray[i].modified=NULL;
+        CacheArray[i].fileNamePointer=EMPTY;
+        CacheArray[i].ID=EMPTY;
+        for(j=0;j<openingSize;j++){
+            CacheArray[i].pins[j]=EMPTY;
+        }                            
+    }
 }
+
+int FileExists(char* filename){  //ELEGXOS IPARXIS ARXEIOU
+    FILE * file=fopen(filename,"r"); 
+    if(file==NULL){
+        fclose(file);
+        return FALSE;
+    }
+        
+    else{
+        fclose(file);
+        return TRUE;   
+    }
+}
+
+int FileIsOpen(char *filename){  //ELEGXOS GIA ANOIXTO ARXEIO
+    int i,j;
+    int counter; //METRITIS GIA DIEUKOLINSI
+    for(i=0;i<openingSize;i++){
+        counter=0;
+        for(j=0;j<FileNameLength;j++){
+            if((OpenFile[i].fileName[j]!=filename[j])){  //ELEGXOS XARAKTIRA-XARAKTIRA
+              
+                counter++;
+            }
+            if((counter>0)||((filename[j]==EOF)&&(OpenFile[i].fileName[j]==filename[j])){
+                break;
+            }
+            else if(((filename[j]==EOF)||(OpenFile[i].fileName[j]==EOF))&&(OpenFile[i].fileName[j]!=filename[j]){
+                counter=2; 
+                break;    
+            }
+            
+        }
+        if(counter==0){
+                return TRUE;
+        }        
+    }
+    return FALSE;
+}
+
 
 int BL_CreateFile(char *filename) {
-    
-    
+    FILE * file;
+    if(FileExists(filename)==FALSE){
+        file=fopen(filename,"w");
+        int i;
+        for(i=0;i<blockSize;i++){
+            fputc(NULL,file);  //ARXIKOPOISI TOU BLOCK HEADER
+        }
+        fclose(file);
+        return BLE_OK;
+    }
+        
+    else{
+        return BLE_FILERROR;
+    }
 }
 
-int BL_DestroyFile(char *filename) {
 
+
+
+int BL_DestroyFile(char *filename) {
+    if(FileExists(filename)==TRUE){
+        if(FileIsOpen()){
+            if(!remove(filename))  //DIAGRAFI ARXEIOU
+                return BLE_OK;
+        
+            else
+                return  BLE_FILENERASE; 
+        } 
+        else
+            return BLE_FILENOPEN;                      
+    }
+    else
+        return BLE_FILENEXIST;
 }
 
 int BL_OpenFile(char *filename) {
--- mem_man.h
+++ mem_man.h
@@ -1,6 +1,60 @@
 #ifndef MEMORY_MANAGEMENT_H
 #define MEMORY_MANAGEMENT_H
 
+#define BLE_OK 0
+#define BLE_NOMEM -1
+#define BLE_NOBUF -2
+#define BLE_BLOCKFIXED -3
+#define BLE_BLOCKNOTINBUF -4
+#define BLE_BLOCKINBUF -5
+#define BLE_OS -6
+#define BLE_INVALIDBLOCK -7
+#define BLE_FILEOPEN -8
+#define BLE_FTABFULL -9
+#define BLE_OFTABFULL -10
+#define BLE_FD -11
+#define BLE_EOF -12
+#define BLE_BLOCKFREE -13
+#define BLE_BLOCKUNFIXED -14
+#define BLE_FILERROR -15  //EXTRA DIKA MAS DEFINE
+#define BLE_FILENERASE -16 //"
+#define BLE_FILENOPEN -17  //"
+#define BLE_FILENEXIST -18  //"
+#define FileNameLength 20
+#define blockSize 1024
+#define openingSize 20
+#define TRUE 1
+#define FALSE 0
+#define EMPTY -1 //gia na doume an enas pinakas einai kenos i oxi|einai swsto i oxi?
+
+typedef struct cacheBlock cacheBlock;
+typedef struct openFiles openFilesInfo;
+
+struct cacheBlock{
+
+       char data[blockSize];  //array me 1024 xaraktires gia block
+       int pins[openingSize];  //array gia pins
+       int ID;  //anagnwristiko gia to block
+       int fileNamePointer; //gia onoma arxeiou
+       int modified; //modified int gia 0 i 1
+
+};// struct gia ta block tis cache
+
+struct openFiles{
+       
+       char fileName[FileNameLength];  //filename
+       char byteMap[blockSize]; //data gia header
+       FILE *fileHandler;  //handler 
+
+};// struct gia ta openfiles
+
+int appOpenings[openingSize]; //app-openings array
+
+openFilesInfo OpenFile[blockSize]; //open files array
+
+cacheBlock CacheArray[openingSize];
+
+
 int BL_init(void);
 
 int BL_DestroyFile(char *filename);
